3035170b68f672ac2777f7a6c9d8d83a86f54d2d
Manish Anand <manishanandprogrammer@gmail.com>
2021-06-12 20:30:19 +0530

    movie info is now coming from theMovieDb website

--- src/main/java/com/example/controller/MovieController.java
+++ src/main/java/com/example/controller/MovieController.java
@@ -1,17 +1,29 @@
 package com.example.controller;
 
 import com.example.model.Movie;
+import com.example.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{movieId}")
     public Movie getMovie(@PathVariable final String movieId) {
-        return new Movie(movieId, "myMovie " + movieId);
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/"+ movieId + "?api_key="+ apiKey,
+                MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 }
--- src/main/java/com/example/model/Movie.java
+++ src/main/java/com/example/model/Movie.java
@@ -3,12 +3,14 @@ package com.example.model;
 public class Movie {
     private String id;
     private String name;
+    private String description;
 
     public Movie() {}
 
-    public Movie(final String id, final String name) {
+    public Movie(final String id, final String name, final String description) {
         this.id = id;
         this.name = name;
+        this.description = description;
     }
 
     public String getId() {
@@ -17,5 +19,9 @@ public class Movie {
 
     public String getName() {
         return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
